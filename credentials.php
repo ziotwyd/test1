083b451e0b0563b4dd6114e0f53d24bd1e587208
ZioT <130393939+ziotwyd@users.noreply.github.com>
2023-11-25 17:07:10 -0500

    Delete credentials.php

--- credentials.php
+++
@@ -1,5 +0,0 @@
-<?php
-$name = "Astrid:CS"; // Application name
-$OwnerId = "7cbibjbQl8"; // Application ownerID
-$SellerKey = "6221027561ba75f6f623f9f01e159eba"; // application's seller key. Found in seller settings (You must have seller plan)
-?>
